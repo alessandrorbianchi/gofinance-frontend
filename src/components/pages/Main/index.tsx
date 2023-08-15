0a3047585ace3b0276539a99a053f76b39fbc8f5
Alessandro Bianchi <alessandro.bianchi@db1.com.br>
2023-08-15 20:29:36 -0300

    feat:Creating a graph of the number of accounts in the home

--- src/components/pages/Main/index.tsx
+++ src/components/pages/Main/index.tsx
@@ -1,12 +1,32 @@
 import Card from '@/components/Card'
 import * as S from './styles'
 import Button from '@/components/Button'
+import { VictoryPie } from 'victory'
 
 const Main = () => (
   <S.Wrapper>
     <S.Main>
       <S.Graph>
-        graph
+      <VictoryPie
+          padAngle={4}
+          animate={{
+            duration: 2000
+          }}
+          data={[
+            { x: "A Pagar", y: 20 },
+            { x: "A Receber", y: 80 },
+          ]}
+          innerRadius={100}
+          colorScale={["tomato", "orange"]}
+          style={{
+            data: {
+              fillOpacity: 0.9, stroke: "#fff", strokeWidth: 3
+            },
+            labels: {
+              fill: "#000",
+            }
+          }}
+        />
       </S.Graph>
       <S.Cards>
         <Card title='Saldo a pagar' count='120.55'/>
